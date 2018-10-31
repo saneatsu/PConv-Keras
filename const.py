4d6d8e96a1efc993bc2509affdf7aeea373e06b6
saneatus <w.saneatsu@gmail.com>
2018-10-31 19:26:29 +0900

    Update for using GPU

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 # NOTE: You need to check directory's permission
 
-MAX_HEIGHT = 2688
-MAX_WIDTH  = 5376
+MAX_HEIGHT = 1536 # 1536 # 1280 # 1024 # 2048 # 2688
+MAX_WIDTH  = 3072 # 3072 # 2560 # 2048 # 4096 # 5376
 
 CROP_HEIGHT = 512 # 256, 512
 CROP_WIDTH  = 1024 # 512, 1024
@@ -13,11 +13,11 @@ ORIGINAL_PATH = ['{}/house-dataset-src/original/'.format(MNT_PATH)]
 RESIZED_PATH  = ['{}/house-dataset-src/resized-2688-5376/'.format(MNT_PATH)]
 
 # house-dataset dir
-TRAIN_PATH    = '{}/house-dataset/train-crop-256-512'.format(MNT_PATH)
-VAL_PATH      = '{}/house-dataset/valid-crop-256-512'.format(MNT_PATH)
-TEST_PATH     = '{}/house-dataset/test-crop-256-512'.format(MNT_PATH)
+TRAIN_PATH    = '/mnt/PConv-Keras/house-dataset/train-crop-256-512'
+VAL_PATH      = '/mnt/PConv-Keras/house-dataset/valid-crop-256-512'
+TEST_PATH     = '/mnt/PConv-Keras/house-dataset/test-crop-256-512'
 
 # data dir
-WEIGHT_PATH   = '{}/data/model/weight-crop-256-512/'.format(MNT_PATH)
+WEIGHT_PATH   = '{}/data/model/weight-crop-512-1024/'.format(MNT_PATH)
 TFLOG_PATH    = '{}/data/model/tflogs'.format(MNT_PATH)
 ERRLOG_PATH   = '{}/error_log/'.format(MNT_PATH)
